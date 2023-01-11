644807e69cda26d9d0f837ddf6fe5b1a8813bf9e
xaviguasch <xaviguasch@gmail.com>
2023-01-11 13:21:07 +0100

    refactored solution to 08-dynamic-colors using closures

--- challenges/08-dynamic-colors/dynamic-colors.js
+++ challenges/08-dynamic-colors/dynamic-colors.js
@@ -2,12 +2,28 @@ const blueBtn = document.querySelector('#blue')
 const pinkBtn = document.querySelector('#pink')
 const greenBtn = document.querySelector('#green')
 
+// const changeColor = (color) => {
+//   console.log(color)
+//   document.body.style.backgroundColor = color
+// }
+
+// // you could pass the hexadecimal strings, instead: "#0f62fe"
+// blueBtn.addEventListener('click', (e) => changeColor(e.target.id))
+// pinkBtn.addEventListener('click', (e) => changeColor(e.target.id))
+// greenBtn.addEventListener('click', (e) => changeColor(e.target.id))
+
+// USING CLOSURES, refactor
+
 const changeColor = (color) => {
-  console.log(color)
-  document.body.style.backgroundColor = color
+  return function () {
+    document.body.style.background = color
+  }
 }
 
-// you could pass the hexadecimal strings, instead: "#0f62fe"
-blueBtn.addEventListener('click', (e) => changeColor(e.target.id))
-pinkBtn.addEventListener('click', (e) => changeColor(e.target.id))
-greenBtn.addEventListener('click', (e) => changeColor(e.target.id))
+const bgColorBlue = changeColor('#0f62fe')
+const bgColorPink = changeColor('#ff7eb6')
+const bgColorGreen = changeColor('#42be65')
+
+blueBtn.addEventListener('click', bgColorBlue)
+pinkBtn.addEventListener('click', bgColorPink)
+greenBtn.addEventListener('click', bgColorGreen)
